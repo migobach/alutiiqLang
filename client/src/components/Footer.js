dcea77c9d479e5be9e4d5d431eb3893537e29440
Michael Bach <migobach@gmail.com>
2018-07-10 21:52:56 -0600

    item glichy

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Container, Grid, Image } from 'semantic-ui-react';
+import { Container, Grid, Image, Item } from 'semantic-ui-react';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import {
+  SubSectionHead,
+  SpecialDiv,
+  ContentStyle,
+} from './styles/CommonStyles'
 import AKHF from '../logos/AKHF.jpg'
 import ANA from '../logos/ANAlogo.png'
 import ANC from '../logos/ANC.jpg'
@@ -21,12 +26,37 @@ const Footer = () => (
       <Grid>
       <Grid.Row centered columns={2} divided>
         <Grid.Column>
-          Contact Us:
+          <Item>
+            <Item.Content header='Contact Us'>
+            <Item.Meta>
+              Native Village of Afognak
+              323 Carolyn Street
+              Kodiak, Alaska 99615
+            </Item.Meta>
+            </Item.Content>
+          </Item>
+          {/* <SpecialDiv>
+            <SubSectionHead>
+              Contact Us:
+            </SubSectionHead>
+            <ContentStyle>
+              Native Village of Afognak
+              <Divider hidden />
+              323 Carolyn Street
+              Kodiak, Alaska 99615
+              <Divider hidden />
+
+              907.486.6357
+            </ContentStyle>
+          </SpecialDiv> */}
         </Grid.Column>
         <Grid.Column>
           Who we are: 
         </Grid.Column>
       </Grid.Row>
+
+      {/* logos of sponsors */}
+
       <Grid.Row columns={10} >
         <Grid.Column >
           <Image src={AKHF} size='tiny'  />
