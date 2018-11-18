ab28d6b3e8b3712ef533c13218e97e06d7f96e54
Michael Bach <migobach@gmail.com>
2018-11-17 18:31:51 -0900

    erinarpet controller

--- app/controllers/api/erinarpets_controller.rb
+++ app/controllers/api/erinarpets_controller.rb
@@ -1,13 +1,47 @@
 class Api::ErinarpetsController < ApplicationController
+  before_action :set_erinarpets, only: [:show]
+
   def index
+    render json: Erinarpet.all
   end
 
   def show
+    render json: @erinarpet
   end
 
   def create
+    erinarpet = Erinarpet.create(erinarpet_params)
+
+    if erinarpet.save
+      render json: erinarpet
+    else
+      render json: { errors: erinarpet.errors.full_message.join(',')}
+    end 
   end
  
   def update
+    if @erinarpet.update(erinarpet_params)
+      render json: @erinarpet
+    else
+      render json: { errors: erinarpet.errors.full_message.join(',')}
+    end
   end
+
+    private 
+
+    def set_erinarpets
+      @erinarpet = Erinarpet.find(params[:id])
+    end
+
+    def erinarpet_params
+      params.require(:erinarpet).permit(
+        :print_date,
+        :topic,
+        :author, 
+        :article_pdf,
+        :image,
+        :notes
+      )
+    end
+
 end
