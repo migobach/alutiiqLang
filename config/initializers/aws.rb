675b9ada4e3f24f0bf67a2f8b804e18b0cb77ef1
Michael Bach <migobach@gmail.com>
2019-03-14 13:15:21 -0800

    update aws

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,6 @@
 Aws.config.update({
   region: 'us-west-2',
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'], region:'us-west-2'),
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+  key = OpenSSL::PKey::RSA.new(1024)
+  s3 = Aws::S3::Encryption::Client.new(encryption_key: key, region: "us-west-2")
 })
-
