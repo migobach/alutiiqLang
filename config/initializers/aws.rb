a20180edb38834d053ca3f1b2e95e35c90cfbf55
Michael Bach <migobach@gmail.com>
2019-03-14 13:41:38 -0800

    add buildpack

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,4 @@
 Aws.config.update({
   region: 'us-west-2',
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-  key = OpenSSL::PKey::RSA.new(1024)
-  s3 = Aws::S3::Encryption::Client.new(encryption_key: key, region: "us-west-2")
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
 })
