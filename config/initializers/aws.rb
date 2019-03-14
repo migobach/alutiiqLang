5afd7eac5f0a88b4b43ccd3ef15760f7439481a5
Michael Bach <migobach@gmail.com>
2019-03-14 12:45:03 -0800

    add region to aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,5 @@
 Aws.config.update({
   region: 'us-west-2',
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'], region:'us-west-2'),
 })
 
