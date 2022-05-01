02f6f29f459765688cf1c6ac40df565875a4436c
Michael Bach <migobach@gmail.com>
2022-05-01 10:14:48 -0800

    try to get migration files to heroku

--- db/migrate/20220430235112_add_columns_to_dictionary.rb
+++ db/migrate/20220430235112_add_columns_to_dictionary.rb
@@ -4,5 +4,6 @@ class AddColumnsToDictionary < ActiveRecord::Migration[5.2]
     add_column :dictionaries, :examples_conjugation_irregulars, :string
     add_column :dictionaries, :negatives, :string
     add_column :dictionaries, :literal_translation, :string
+
   end
 end
