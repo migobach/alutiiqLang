8319c422b79e6dc974d14ef2468f96ae640e454a
Michael Bach <migobach@gmail.com>
2022-05-10 09:05:17 -0800

    clean up form for classes and home pages

--- client/src/components/ItemForm.js
+++ client/src/components/ItemForm.js
@@ -25,8 +25,8 @@ class ItemForm extends Component {
 
   componentDidMount() {
     if (this.props.items)
-    console.log('in the form comp', this.props.items)
-      this.setState({...this.props.items})
+      console.log('in the form comp', this.props.items)
+    this.setState({ ...this.props.items })
   }
 
   handleChange = (e, { name, value }) => {
@@ -36,12 +36,12 @@ class ItemForm extends Component {
   handleSubmit = (e) => {
     const { dispatch, toggleForm } = this.props
     e.preventDefault()
-      this.setState({ page: this.props.originPage }, () => {
-        console.log('originPage:', this.props.originPage, '\n', {...this.state})
-        let itemData = {...this.state}
-        dispatch(addItem(itemData))
-        toggleForm()
-      }
+    this.setState({ page: this.props.originPage }, () => {
+      console.log('originPage:', this.props.originPage, '\n', { ...this.state })
+      let itemData = { ...this.state }
+      dispatch(addItem(itemData))
+      toggleForm()
+    }
     )
   }
 
@@ -51,7 +51,7 @@ class ItemForm extends Component {
 
     if (user.id) {
 
-      return(
+      return (
         <SpecialDiv>
           <SpecialDiv>
             <Form onSubmit={this.handleSubmit}>
@@ -66,63 +66,72 @@ class ItemForm extends Component {
                   onChange={this.handleChange}
                 />
               </Form.Group>
-                <Divider hidden/>
+              <Divider hidden />
               <Form.Group widths='equal'>
                 <Form.TextArea
-                    label='Insert the content here. Keep it brief.'
-                    name='body'
-                    value={body}
-                    placeholder='Content...'
-                    required
-                    onChange={this.handleChange}
-                  />
-              </Form.Group>
-
-              <Form.Group widths='equal'>
-                <Form.TextArea
-                    label='If there is a location you want to share, put it here.'
-                    name='location'
-                    value={location}
-                    placeholder='Ex: 612 Egan Way, Kodiak, AK 99615'
-                    onChange={this.handleChange}
-                  />
-              </Form.Group>
-
-              <Form.Group widths='equal'>
-                <Form.TextArea
-                    label='Is there a point of contact to share?'
-                    name='contact'
-                    value={contact}
-                    placeholder='Call 907-486-4449 for more information'
-                    onChange={this.handleChange}
-                  />
-              </Form.Group>
-
-                <Divider hidden/>
-              <Form.Group widths='equal'>
-                <Form.Input
-                  label='What is the link for the button?'
-                  name='buttonUrl'
-                  value={buttonUrl}
-                  placeholder='https:www.example.com OR mailto:myemail@email.com'
-                  required
-                  onChange={this.handleChange}
-                />
-                <Form.Input
-                  label='Button name'
-                  name='buttonName'
-                  value={buttonName}
-                  placeholder='Button name...'
+                  label='Insert the content here. Keep it brief.'
+                  name='body'
+                  value={body}
+                  placeholder='Content...'
                   required
                   onChange={this.handleChange}
                 />
               </Form.Group>
-                {/* <Form.Checkbox
-                  label='Is this segment going to be visible?'
-                  value={visible}
-                  onChange={this.handleBoolean}
-                /> */}
-                <Divider hidden />
+              {this.props.originPage === 'classes' ?
+                <div>
+                  <Form.Group widths='equal'>
+                    <Form.TextArea
+                      label='If there is a location you want to share, put it here.'
+                      name='location'
+                      value={location}
+                      placeholder='Ex: 612 Egan Way, Kodiak, AK 99615'
+                      onChange={this.handleChange}
+                    />
+                  </Form.Group>
+
+                  <Form.Group widths='equal'>
+                    <Form.TextArea
+                      label='Is there a point of contact to share?'
+                      name='contact'
+                      value={contact}
+                      placeholder='Call 907-486-4449 for more information'
+                      onChange={this.handleChange}
+                    />
+                  </Form.Group>
+                </div>
+                :
+                null
+
+              }
+
+              <Divider hidden />
+
+              {this.props.originPage === 'home' ?
+                <div>
+                  <Form.Group widths='equal'>
+                    <Form.Input
+                      label='What is the link for the button?'
+                      name='buttonUrl'
+                      value={buttonUrl}
+                      placeholder='https:www.example.com OR mailto:myemail@email.com'
+                      required
+                      onChange={this.handleChange}
+                    />
+                    <Form.Input
+                      label='Button name'
+                      name='buttonName'
+                      value={buttonName}
+                      placeholder='Button name...'
+                      required
+                      onChange={this.handleChange}
+                    />
+                  </Form.Group>
+                </div>
+                :
+                null
+              }
+
+              <Divider hidden />
               <Button type='submit'>
                 Save
               </Button>
@@ -136,20 +145,20 @@ class ItemForm extends Component {
         </SpecialDiv>
       )
     } else {
-      return(
+      return (
         null
       )
     }
   }
 
 
-    render() {
-      return(
-        <div>
-          { this.handleForm() }
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div>
+        {this.handleForm()}
+      </div>
+    )
+  }
 }
 
 export default connect()(ItemForm)
